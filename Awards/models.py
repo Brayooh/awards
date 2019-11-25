00e2b6d22c78e64c26cc55ab7efb76326e216810
brayooh <brianndichu.bn@gmail.com>
2019-11-25 10:20:13 +0300

    create project class

--- Awards/models.py
+++ Awards/models.py
@@ -28,3 +28,14 @@ class Profile(models.Model):
         self.save()
 
 
+
+class Projects(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(upload_to='profile_pics/')
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    author_profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default='1', blank = True)
+
+
