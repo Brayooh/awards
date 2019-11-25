daa057ebf8133a3a26e784f4e55c82554dd864af
brayooh <brianndichu.bn@gmail.com>
2019-11-25 18:36:27 +0300

    modify settings

--- Projects/settings.py
+++ Projects/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$v-m+6%j95f42od0x3ypsf5w#7fbs6kgviu&qp%kq7rw(86zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS='.localhost', '.herokuapp.com', '.127.0.0.1'
 DISABLE_COLLECTSTATIC=1
 
