61062c5981557bedc608c0783507382b1da94a87
brayooh <brianndichu.bn@gmail.com>
2019-11-25 10:20:45 +0300

    add tests to project class

--- Awards/models.py
+++ Awards/models.py
@@ -38,4 +38,28 @@ class Projects(models.Model):
     link = models.URLField()
     author_profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default='1', blank = True)
 
+    def save_project(self):
+        self.save()
 
+    def __str__(self):
+        return f'{self.author} Post'
+
+    class Meta:
+        db_table = 'project'
+        ordering = ['-created_date']
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def search_projects(cls,search_term):
+        project = cls.objects.filter(title__icontains=search_term)
+        return project
+
+    @classmethod
+    def get_project(cls,id):
+        try:
+            project = Projects.objects.get(pk=id)
+        except ObjectDoesNotExist:
+            raise Http404()
+        return Project
